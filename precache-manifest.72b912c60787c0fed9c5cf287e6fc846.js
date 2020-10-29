8bcef8da27299f27656bd85b8e23085e49b313ec
Walter <walter@medialand.tw>
2020-10-30 00:42:48 +0800

    Updates

---
+++ precache-manifest.72b912c60787c0fed9c5cf287e6fc846.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5ef4aac6088ede3637c2d99f7c71147",
+    "url": "/Phonetic_Symbols/index.html"
+  },
+  {
+    "revision": "52f9b64752d4c6954d94",
+    "url": "/Phonetic_Symbols/static/css/3.96cb01f0.chunk.css"
+  },
+  {
+    "revision": "c5e70c65db21f45eff25",
+    "url": "/Phonetic_Symbols/static/css/main.1d24bce3.chunk.css"
+  },
+  {
+    "revision": "4a3ace14de0bbdfe6caa",
+    "url": "/Phonetic_Symbols/static/js/2.245710f5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Phonetic_Symbols/static/js/2.245710f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52f9b64752d4c6954d94",
+    "url": "/Phonetic_Symbols/static/js/3.feac8152.chunk.js"
+  },
+  {
+    "revision": "1dd5d9a3e23249802510",
+    "url": "/Phonetic_Symbols/static/js/4.74b2ee03.chunk.js"
+  },
+  {
+    "revision": "275fe79abee3b697f1673c8bd9c58856",
+    "url": "/Phonetic_Symbols/static/js/4.74b2ee03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5e70c65db21f45eff25",
+    "url": "/Phonetic_Symbols/static/js/main.0bc1dd8e.chunk.js"
+  },
+  {
+    "revision": "bc33bc5d98324279eeea",
+    "url": "/Phonetic_Symbols/static/js/runtime-main.1976d290.js"
+  }
+]);
